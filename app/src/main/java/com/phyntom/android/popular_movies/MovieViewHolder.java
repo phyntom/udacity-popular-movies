9df00462b4edfa88fb17a2c1b27c909edcf780b5
Aimable Ruhumuliza <phyntom@gmail.com>
2017-10-02 19:52:29 +0200

    Cleaning up the popular movie app

--- app/src/main/java/com/phyntom/android/popular_movies/MovieViewHolder.java
+++ app/src/main/java/com/phyntom/android/popular_movies/MovieViewHolder.java
@@ -17,7 +17,7 @@ public class MovieViewHolder extends RecyclerView.ViewHolder {
 
     public MovieViewHolder(View itemView) {
         super(itemView);
-        posterImageView = (ImageView) itemView.findViewById(R.id.iv_video_poster);
+        posterImageView = itemView.findViewById(R.id.iv_video_poster);
     }
 
     public void bind(final Movie movie, final MovieViewHolderClickListener listener){
